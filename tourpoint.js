555cb01b0915a7cda49fb0e2d7a8d3560e386b25
thanyapornn <158013827+thanyapornn@users.noreply.github.com>
2024-05-13 16:37:43 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,156 @@
+var tourpoint =
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Yai National Park",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            101.49605708067509,
+            14.330875479679307
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Grand Palace",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.49160084232852,
+            13.751024414597154
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Koh Sirey beach",
+          "type": "natural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            98.43953725273832,
+            7.889259944640884
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ko Chang",
+          "type": "natural",
+          "level": 5
+        },
+        "geometry": {
+          "coordinates": [
+            102.33859845974149,
+            12.045147869514977
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "khao Kheow  Open Zoo",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            101.05938023195301,
+            13.21332024809449
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Siam Center",
+          "type": " building",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.532819071164,
+            13.746153834456592
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chulalong Univeraity Centennial Park",
+          "type": "natural",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            100.52404368175013,
+            13.739422553904802
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Maekampong Museum",
+          "type": " building",
+          "level": 3
+        },
+        "geometry": {
+          "coordinates": [
+            99.34905402934788,
+            18.86569778914729
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": " Wat Arun Rajwararam",
+          "type": "cutural",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.48871728836332,
+            13.744546210805709
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Flatbread",
+          "type": " building",
+          "level": 4
+        },
+        "geometry": {
+          "coordinates": [
+            100.48613647699392,
+            13.745278244367412
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
